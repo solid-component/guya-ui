36e26fa529aef81947b12a8e40d34d27b313eaad
Quan <15577781705@163.com>
2024-04-22 11:53:23 +0800

    test commit

---
+++ assets/installation-CogAsq6f.js
@@ -0,0 +1,20 @@
+import{M as l,c as e,m as i}from"./index-CR7AtsoQ.js";function r(s){const n={code:"code",h1:"h1",p:"p",pre:"pre",span:"span",...l(),...s.components};return[e(n.h1,{children:"说明"}),`
+`,e(n.p,{children:"正式版本未发布"}),`
+`,e(n.p,{children:`个人开发，需要做的东西很多，需要做主题处理，国际化处理，设计将参考element-plus和ant-design，
+目前只是做了些简单的demo供参考，未来会逐渐完善功能
+得益于solid-js的signal的细粒度更新，直接使用css-in-js。`}),`
+`,e(n.h1,{children:"安装"}),`
+`,e(n.pre,{className:"shiki shiki-themes vitesse-light vitesse-dark",style:{backgroundColor:"#ffffff","--shiki-dark-bg":"#121212",color:"#393a34","--shiki-dark":"#dbd7caee"},tabIndex:"0",get children(){return e(n.code,{get children(){return[e(n.span,{className:"line",get children(){return e(n.span,{style:{color:"#A0ADA0","--shiki-dark":"#758575DD"},children:"# 选择一个你喜欢的包管理器"})}}),`
+`,e(n.span,{className:"line"}),`
+`,e(n.span,{className:"line",get children(){return e(n.span,{style:{color:"#A0ADA0","--shiki-dark":"#758575DD"},children:"# NPM"})}}),`
+`,e(n.span,{className:"line",get children(){return[e(n.span,{style:{color:"#59873A","--shiki-dark":"#80A665"},children:"$"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" npm"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" install"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" guya-ui"}),e(n.span,{style:{color:"#A65E2B","--shiki-dark":"#C99076"},children:" --save"})]}}),`
+`,e(n.span,{className:"line"}),`
+`,e(n.span,{className:"line",get children(){return e(n.span,{style:{color:"#A0ADA0","--shiki-dark":"#758575DD"},children:"# Yarn"})}}),`
+`,e(n.span,{className:"line",get children(){return[e(n.span,{style:{color:"#59873A","--shiki-dark":"#80A665"},children:"$"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" yarn"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" add"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" guya-ui"})]}}),`
+`,e(n.span,{className:"line"}),`
+`,e(n.span,{className:"line",get children(){return e(n.span,{style:{color:"#A0ADA0","--shiki-dark":"#758575DD"},children:"# pnpm"})}}),`
+`,e(n.span,{className:"line",get children(){return[e(n.span,{style:{color:"#59873A","--shiki-dark":"#80A665"},children:"$"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" pnpm"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" install"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" guya-ui"})]}}),`
+`,e(n.span,{className:"line"}),`
+`,e(n.span,{className:"line",get children(){return e(n.span,{style:{color:"#A0ADA0","--shiki-dark":"#758575DD"},children:"# bun"})}}),`
+`,e(n.span,{className:"line",get children(){return[e(n.span,{style:{color:"#59873A","--shiki-dark":"#80A665"},children:"$"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" bun"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" add"}),e(n.span,{style:{color:"#B56959","--shiki-dark":"#C98A7D"},children:" guya-ui"})]}}),`
+`,e(n.span,{className:"line"})]}})}})]}function c(s={}){const{wrapper:n}={...l(),...s.components};return n?e(n,i(s,{get children(){return e(r,s)}})):r(s)}export{c as default};
