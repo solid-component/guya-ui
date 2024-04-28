d69f6328b698ea2f5eeca6673a2cf2375cdb30ba
Quan <15577781705@163.com>
2024-04-28 19:25:26 +0800

    test commit

--- assets/changelog-D1LSmfP6.js
+++ assets/changelog-D1LSmfP6.js
@@ -1,4 +1,4 @@
-import{M as r,c as e,m as a}from"./index-CGWGu_bD.js";function c(t){const n=Object.assign({h1:"h1",p:"p"},r(),t.components);return[e(n.h1,{id:"说明",children:"说明"}),`
+import{M as r,c as e,m as a}from"./index-CYg3LTZf.js";function c(t){const n=Object.assign({h1:"h1",p:"p"},r(),t.components);return[e(n.h1,{id:"说明",children:"说明"}),`
 `,e(n.p,{children:"Guya UI 目前还处于快速开发迭代中。"}),`
 `,e(n.p,{children:"设计参考element-plus和ant-design"}),`
 `,e(n.h1,{id:"001-canary",children:"0.0.1-canary"}),`
