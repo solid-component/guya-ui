329d4e5eebf918b3b215813a71c3c7cf6fab1e04
Quan <15577781705@163.com>
2024-04-28 15:45:09 +0800

    test commit

---
+++ assets/changelog-DywshsUk.js
@@ -0,0 +1,5 @@
+import{M as c,l as e,q as a}from"./index-CEbFxx_N.js";function r(t){const n=Object.assign({h1:"h1",p:"p"},c(),t.components);return[e(n.h1,{id:"说明",children:"说明"}),`
+`,e(n.p,{children:"Guya UI 目前还处于快速开发迭代中。"}),`
+`,e(n.p,{children:"设计参考element-plus和ant-design"}),`
+`,e(n.h1,{id:"001-canary",children:"0.0.1-canary"}),`
+`,e(n.p,{children:"测试版"})]}function i(t={}){const{wrapper:n}=Object.assign({},c(),t.components);return n?e(n,a(t,{get children(){return e(r,t)}})):r(t)}export{i as default};
